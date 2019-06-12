85f4e753d6cccaee9697a8a5045a981831e7e305
tahmid <tahmidfarhan12@gmail.com>
2019-06-13 02:33:53 +0600

    1st Commit

---
+++ music/urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from . import views
+from django.urls import path
+
+app_name = 'music' 
+
+urlpatterns = [
+   # music/
+    path('', views.IndexView.as_view() , name = 'index' ) ,
+
+    # music/71/
+   path('<int:pk>/', views.DetailView.as_view(), name='detail' ),
+
+   #music/album/add/
+   path('album/add' , views.AlbumCreate.as_view() , name= 'album-add'  ) ,
+
+    # music/album/71/       #update
+   path('album/<int:pk>/', views.AlbumUpdate.as_view(), name='album-update' ),
+
+# music/album/71/            #delete
+   path('album/<int:pk>/delete', views.AlbumDelete.as_view(), name='album-delete' ),
+  
+   #path('<int:pk>/favorite/', views.favorite, name='favorite' ),
+
+]
